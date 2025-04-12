7b36f09dc153cd5e9a3c1f623964885d0158311b
andres vasquez <andres.vasquez@aldeamo.com>
2025-04-11 22:26:02 -0500

    first commit

---
+++ src/main/java/com/appgate/DistinctSubsequencesApplication.java
@@ -0,0 +1,13 @@
+package com.appgate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistinctSubsequencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistinctSubsequencesApplication.class, args);
+	}
+
+}
